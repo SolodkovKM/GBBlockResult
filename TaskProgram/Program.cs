543b1d43300ad8c9be9740c8162ba86ee2634e65
kost <solodkovkm1997@gmail.com>
2023-03-21 16:06:31 +0300

    Program Writed

---
+++ TaskProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] array = new string[3] {"12354", "432", "as"};
+
+string[] ArrayItemNumDegree(string[] array, int countOfDigits)
+{
+    return array.Where(x=> x.Length <= countOfDigits).ToArray();
+}
+
+Console.WriteLine($"[{string.Join(",",array)}]->[{string.Join(",", ArrayItemNumDegree(array, 3))}]");
+Console.WriteLine($"[{string.Join(",",array)}]->[{string.Join(",", ArrayItemNumDegree(array, 2))}]");
+Console.WriteLine($"[{string.Join(",",array)}]->[{string.Join(",", ArrayItemNumDegree(array, 1))}]");
