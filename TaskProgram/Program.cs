5906c1c633e8645e1ca08e49c2f0f56ea46a9b59
kost <solodkovkm1997@gmail.com>
2023-03-24 00:13:12 +0300

    Rebuild solution

--- TaskProgram/Program.cs
+++ TaskProgram/Program.cs
@@ -25,15 +25,16 @@ string[] ArrayItemNumDegree(string[] array, int countOfDigits) // Вариант
 
 string[] ResizeMethod(string[] array, int countOfDigits) // Вариант через изменение размера массива
 {
-    for(int i = 0; i < array.Length; i++)
+    for(int i = array.Length-1; i >= 0; i--)
     {
-        if(array[i].Length > countOfDigits) 
+        int countOfItems = 0;
+        if(array[i].Length <= countOfDigits) 
         {
-            string temp = array[array.Length - 1];
-            array[array.Length - 1] = array[i];
+            string temp = array[countOfItems];
+            array[countOfItems] = array[i];
             array[i] = temp;
-            Array.Resize(ref array, array.Length - 1);
         }
+        else Array.Resize(ref array, i);
     }
     return array;
 }
@@ -42,7 +43,9 @@ string[] AppendMethod(string[] array, int countOfDigits) // Вариант че�
 {
     string[] result = new string[0];
     for(int i = 0; i < array.Length; i++)
-        if(array[i].Length <= countOfDigits) result.Append(array[i]);
+    {
+        if(array[i].Length <= countOfDigits) result = result.Append(array[i]).ToArray();
+    }
     return result;
 }
 
@@ -50,18 +53,19 @@ string[] array1 = new string[4] {"hello", "2", "world", ":-)"};
 string[] array2 = new string[4] {"1234", "1567", "-2", "computer science"};
 string[] array3 = new string[3] {"Russia", "Denmark", "Kazan"};
 
+Console.WriteLine("ArrayItemNumDegreeLinq");
 Console.WriteLine($"[{string.Join(",",array1)}]->[{string.Join(",", ArrayItemNumDegreeLinq(array1, 3))}]");
 Console.WriteLine($"[{string.Join(",",array2)}]->[{string.Join(",", ArrayItemNumDegreeLinq(array2, 3))}]");
 Console.WriteLine($"[{string.Join(",",array3)}]->[{string.Join(",", ArrayItemNumDegreeLinq(array3, 3))}]");
-Console.WriteLine();
+Console.WriteLine("ArrayItemNumDegree");
 Console.WriteLine($"[{string.Join(",",array1)}]->[{string.Join(",", ArrayItemNumDegree(array1, 3))}]");
 Console.WriteLine($"[{string.Join(",",array2)}]->[{string.Join(",", ArrayItemNumDegree(array2, 3))}]");
 Console.WriteLine($"[{string.Join(",",array3)}]->[{string.Join(",", ArrayItemNumDegree(array3, 3))}]");
-Console.WriteLine();
+Console.WriteLine("ResizeMethod");
 Console.WriteLine($"[{string.Join(",",array1)}]->[{string.Join(",", ResizeMethod(array1, 3))}]");
 Console.WriteLine($"[{string.Join(",",array2)}]->[{string.Join(",", ResizeMethod(array2, 3))}]");
 Console.WriteLine($"[{string.Join(",",array3)}]->[{string.Join(",", ResizeMethod(array3, 3))}]");
-Console.WriteLine();
+Console.WriteLine("AppendMethod");
 Console.WriteLine($"[{string.Join(",",array1)}]->[{string.Join(",", AppendMethod(array1, 3))}]");
 Console.WriteLine($"[{string.Join(",",array2)}]->[{string.Join(",", AppendMethod(array2, 3))}]");
 Console.WriteLine($"[{string.Join(",",array3)}]->[{string.Join(",", AppendMethod(array3, 3))}]");
